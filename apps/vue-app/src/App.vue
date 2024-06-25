19dac6979fb5ed8621bf2c7ac6b137530ae0ab27
Geoffrey Chong <gyf.chong@gmail.com>
2024-06-25 17:24:51 +1000

    added: connection to Apollo to vue app

--- apps/vue-app/src/App.vue
+++ apps/vue-app/src/App.vue
@@ -1,8 +1,38 @@
+<script>
+import gql from "graphql-tag";
+import { useQuery } from "@vue/apollo-composable";
+
+const POSTS_QUERY = gql`
+  query Post {
+    post(id: "apollo-federation") {
+      content
+    }
+  }
+`;
+
+export default {
+  name: "App",
+  setup() {
+    const { result, loading, error } = useQuery(POSTS_QUERY);
+    console.info("result", result);
+    return {
+      result: result.value,
+      loading,
+      error,
+    };
+  },
+};
+</script>
+
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <p v-if="error">Something went wrong...</p>
+  <p v-if="loading">Loading...</p>
+  <p v-if="result.post">{{ result.post.content }}</p>
+  <div></div>
   <router-view />
 </template>
 
--- apps/vue-app/src/main.ts
+++ apps/vue-app/src/main.ts
@@ -1,7 +1,28 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: "https://cixozlje55czlcocuzozpqcaxq.appsync-api.ap-southeast-2.amazonaws.com/graphql",
+  headers: {
+    "x-api-key": "da2-ozcyrvcfqrg7dfmi5kege25bom",
+  },
+});
+
+createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+})
+  .use(store)
+  .use(router)
+  .mount("#app");
