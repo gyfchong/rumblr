331fada6c17f47d9e09ec448b0eb5de57282541b
Geoffrey Chong <gyf.chong@gmail.com>
2024-07-03 09:07:16 +1000

    added: linting

--- apps/vue-project/src/App.vue
+++ apps/vue-project/src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import gql from 'graphql-tag'
-import { useQuery } from '@vue/apollo-composable'
+import { RouterLink, RouterView } from "vue-router"
+import HelloWorld from "./components/HelloWorld.vue"
+import gql from "graphql-tag"
+import { useQuery } from "@vue/apollo-composable"
 
 const POSTS_QUERY = gql`
   query Post {
@@ -16,7 +16,13 @@ const { result, loading, error } = useQuery(POSTS_QUERY)
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
